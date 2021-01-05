39d82c4cf93d19f0eebeed6b61325c7697ecf8a6
juancortelezzi <juanbicortelezzi@gmail.com>
2021-01-05 16:30:53 -0300

    contentful working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
