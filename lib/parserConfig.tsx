4c2d2ef8cbccb45822f3370497dc90afd5c904bc
juancortelezzi <juanbicortelezzi@gmail.com>
2021-01-12 23:12:20 -0300

    small fixes

---
+++ lib/parserConfig.tsx
@@ -0,0 +1,165 @@
+import { Link } from "../components/link";
+import Image from "next/image";
+import { PostCard } from "../components/postCard";
+import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
+import {
+  Heading,
+  Text,
+  Code,
+  ListItem,
+  UnorderedList,
+  OrderedList,
+  Center,
+  Divider,
+  Flex,
+  Spacer,
+  Box,
+} from "@chakra-ui/react";
+
+export const getConfig = (post: any) => ({
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (_node: any, children: any) => <Text fontSize="lg">{children}</Text>,
+    [BLOCKS.HEADING_1]: (node: any) => (
+      <Heading as="h2" size="3xl" lineHeight="tall">
+        {node.content[0].value}
+      </Heading>
+    ),
+    [BLOCKS.HEADING_2]: (node: any) => (
+      <Heading as="h3" size="2xl" lineHeight="tall">
+        {node.content[0].value}
+      </Heading>
+    ),
+    [BLOCKS.HEADING_3]: (node: any) => (
+      <Heading as="h4" size="xl" lineHeight="tall">
+        {node.content[0].value}
+      </Heading>
+    ),
+    [BLOCKS.HEADING_4]: (node: any) => (
+      <Heading as="h5" size="xl" lineHeight="tall">
+        {node.content[0].value}
+      </Heading>
+    ),
+    [BLOCKS.HEADING_5]: (node: any) => (
+      <Heading as="h6" size="xl" lineHeight="tall">
+        {node.content[0].value}
+      </Heading>
+    ),
+    [BLOCKS.HEADING_6]: (node: any) => (
+      <Text fontSize="3xl" fontWeight="semibold" lineHeight="tall">
+        {node.content[0].value}
+      </Text>
+    ),
+    [BLOCKS.UL_LIST]: (_node: any, children: any) => (
+      <Center w="xs" py={4}>
+        <UnorderedList>{children}</UnorderedList>
+      </Center>
+    ),
+    [BLOCKS.OL_LIST]: (_node: any, children: any) => (
+      <Center w="xs" py={4}>
+        <OrderedList>{children}</OrderedList>
+      </Center>
+    ),
+    [BLOCKS.LIST_ITEM]: (node: any) => (
+      <ListItem>
+        <Text fontSize="lg">{node.content[0].content[0].value}</Text>
+      </ListItem>
+    ),
+    [BLOCKS.QUOTE]: (_node: any, children: any) => (
+      <Center my={4}>
+        <Flex
+          w="xs"
+          justify="center"
+          align="center"
+          direction="column"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={4}
+        >
+          <Flex w="full">
+            <Text as="i" fontSize="4xl" fontWeight="bold" align="center">
+              "
+            </Text>
+            <Spacer />
+          </Flex>
+          <Spacer />
+          <Text as="i" fontSize="lg" align="center" w={3 / 4}>
+            {children}
+          </Text>
+          <Flex w="full">
+            <Spacer />
+            <Text as="i" fontSize="4xl" fontWeight="bold" align="center">
+              "
+            </Text>
+          </Flex>
+        </Flex>
+      </Center>
+    ),
+    [BLOCKS.HR]: () => <Divider my={4} />,
+    [BLOCKS.EMBEDDED_ASSET]: (node: any) => {
+      const [image] = post.content.links.assets.block.filter(
+        (n: any) => n.sys.id === node.data.target.sys.id
+      );
+      return (
+        <Center my={4}>
+          <Box w={["xs", "md", "xl", "2xl"]} maxW="2xl" borderRadius="lg" overflow="hidden">
+            <Image
+              src={image.url}
+              width={2000}
+              height={1000}
+              layout="responsive"
+              objectFit="cover"
+            />
+          </Box>
+        </Center>
+      );
+    },
+    [BLOCKS.EMBEDDED_ENTRY]: (node: any) => {
+      const [entry] = post.content.links.entries.block.filter(
+        (n: any) => n.sys.id === node.data.target.sys.id
+      );
+      if (entry.__typename === "Post") {
+        return (
+          <Center py={4}>
+            <PostCard p={entry} />
+          </Center>
+        );
+      } else {
+        return <Text>NOT DONE YET</Text>;
+      }
+    },
+    [INLINES.HYPERLINK]: (node: any) => (
+      <Link href={node.data.uri} color="teal.500">
+        {node.content[0].value}
+      </Link>
+    ),
+    [INLINES.EMBEDDED_ENTRY]: (node: any) => {
+      const [entry] = post.content.links.entries.inline.filter(
+        (n: any) => n.sys.id === node.data.target.sys.id
+      );
+      return (
+        <Link href={`/posts/${entry.slug}`} color="teal.500">
+          '{entry.title}' by {entry.author.name}
+        </Link>
+      );
+    },
+  },
+  renderMark: {
+    [MARKS.BOLD]: (text: any) => (
+      <Text as="b" fontSize="lg" fontWeight="bold">
+        {text}
+      </Text>
+    ),
+    [MARKS.CODE]: (text: any) => (
+      <Flex as="span" display="block" my={4} justify="center" align="center" direction="column">
+        <Code fontSize="lg" display="block">
+          {text}
+        </Code>
+      </Flex>
+    ),
+  },
+  renderText: (text: any) => {
+    return text.split("\n").reduce((children: any[], textSegment: any, index: number) => {
+      return [...children, index > 0 && <br key={index} />, textSegment];
+    }, []);
+  },
+});
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,27 +1,20 @@
 import { initializeApollo, createApolloClient } from "../../lib/apolloClient";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
 import ErrorPage from "next/error";
 import Image from "next/image";
 import Head from "next/head";
 import { GetStaticProps } from "next";
-import { PostCard } from "../../components/postCard";
+import { useQuery } from "@apollo/client";
 import { parseDate } from "../../lib/parseDate";
 import ThemeSwitch from "../../components/themeSwitch";
+import { getConfig } from "../../lib/parserConfig";
 import { Link } from "../../components/link";
 import { getPostData, getPreviewPostData, getPostSlug, IGetPostSlug } from "../../lib/apolloQuerys";
-import { useQuery } from "@apollo/client";
 import {
   Heading,
   Text,
   Avatar,
   HStack,
-  Code,
-  ListItem,
-  UnorderedList,
-  OrderedList,
-  Center,
-  Divider,
   Flex,
   Spacer,
   Skeleton,
@@ -32,8 +25,8 @@ import {
 
 export default function Post({
   slug,
-  preview,
-  previewPost,
+  preview = false,
+  previewPost = null,
 }: {
   slug: string;
   preview: boolean;
@@ -46,11 +39,7 @@ export default function Post({
   if (preview === true) {
     loading = false;
     post = previewPost;
-    if (!previewPost) {
-      error = true;
-    } else {
-      error = undefined;
-    }
+    if (!previewPost) error = true;
   } else {
     const { loading: Loading, error: Error, data: Data } = useQuery(getPostData, {
       variables: { slug },
@@ -63,154 +52,6 @@ export default function Post({
   if (!loading && (error || !post)) {
     return <ErrorPage statusCode={404} />;
   }
-
-  const config = {
-    renderNode: {
-      [BLOCKS.PARAGRAPH]: (_node: any, children: any) => <Text fontSize="lg">{children}</Text>,
-      [BLOCKS.HEADING_1]: (node: any) => (
-        <Heading as="h2" size="3xl" lineHeight="tall">
-          {node.content[0].value}
-        </Heading>
-      ),
-      [BLOCKS.HEADING_2]: (node: any) => (
-        <Heading as="h3" size="2xl" lineHeight="tall">
-          {node.content[0].value}
-        </Heading>
-      ),
-      [BLOCKS.HEADING_3]: (node: any) => (
-        <Heading as="h4" size="xl" lineHeight="tall">
-          {node.content[0].value}
-        </Heading>
-      ),
-      [BLOCKS.HEADING_4]: (node: any) => (
-        <Heading as="h5" size="xl" lineHeight="tall">
-          {node.content[0].value}
-        </Heading>
-      ),
-      [BLOCKS.HEADING_5]: (node: any) => (
-        <Heading as="h6" size="xl" lineHeight="tall">
-          {node.content[0].value}
-        </Heading>
-      ),
-      [BLOCKS.HEADING_6]: (node: any) => (
-        <Text fontSize="3xl" fontWeight="semibold" lineHeight="tall">
-          {node.content[0].value}
-        </Text>
-      ),
-      [BLOCKS.UL_LIST]: (_node: any, children: any) => (
-        <Center w="xs" py={4}>
-          <UnorderedList>{children}</UnorderedList>
-        </Center>
-      ),
-      [BLOCKS.OL_LIST]: (_node: any, children: any) => (
-        <Center w="xs" py={4}>
-          <OrderedList>{children}</OrderedList>
-        </Center>
-      ),
-      [BLOCKS.LIST_ITEM]: (node: any) => (
-        <ListItem>
-          <Text fontSize="lg">{node.content[0].content[0].value}</Text>
-        </ListItem>
-      ),
-      [BLOCKS.QUOTE]: (_node: any, children: any) => (
-        <Center my={4}>
-          <Flex
-            w="xs"
-            justify="center"
-            align="center"
-            direction="column"
-            borderWidth="1px"
-            borderRadius="lg"
-            p={4}
-          >
-            <Flex w="full">
-              <Text as="i" fontSize="4xl" fontWeight="bold" align="center">
-                "
-              </Text>
-              <Spacer />
-            </Flex>
-            <Spacer />
-            <Text as="i" fontSize="lg" align="center" w={3 / 4}>
-              {children}
-            </Text>
-            <Flex w="full">
-              <Spacer />
-              <Text as="i" fontSize="4xl" fontWeight="bold" align="center">
-                "
-              </Text>
-            </Flex>
-          </Flex>
-        </Center>
-      ),
-      [BLOCKS.HR]: () => <Divider my={4} />,
-      [BLOCKS.EMBEDDED_ASSET]: (node: any) => {
-        const [image] = post.content.links.assets.block.filter(
-          (n: any) => n.sys.id === node.data.target.sys.id
-        );
-        return (
-          <Center my={4}>
-            <Box w={["xs", "md", "xl", "2xl"]} maxW="2xl" borderRadius="lg" overflow="hidden">
-              <Image
-                src={image.url}
-                width={2000}
-                height={1000}
-                layout="responsive"
-                objectFit="cover"
-              />
-            </Box>
-          </Center>
-        );
-      },
-      [BLOCKS.EMBEDDED_ENTRY]: (node: any) => {
-        const [entry] = post.content.links.entries.block.filter(
-          (n: any) => n.sys.id === node.data.target.sys.id
-        );
-        if (entry.__typename === "Post") {
-          return (
-            <Center py={4}>
-              <PostCard p={entry} />
-            </Center>
-          );
-        } else {
-          return <Text>NOT DONE YET</Text>;
-        }
-      },
-      [INLINES.HYPERLINK]: (node: any) => (
-        <Link href={node.data.uri} color="teal.500">
-          {node.content[0].value}
-        </Link>
-      ),
-      [INLINES.EMBEDDED_ENTRY]: (node: any) => {
-        const [entry] = post.content.links.entries.inline.filter(
-          (n: any) => n.sys.id === node.data.target.sys.id
-        );
-        return (
-          <Link href={`/posts/${entry.slug}`} color="teal.500">
-            '{entry.title}' by {entry.author.name}
-          </Link>
-        );
-      },
-    },
-    renderMark: {
-      [MARKS.BOLD]: (text: any) => (
-        <Text as="b" fontSize="lg" fontWeight="bold">
-          {text}
-        </Text>
-      ),
-      [MARKS.CODE]: (text: any) => (
-        <Flex as="span" display="block" my={4} justify="center" align="center" direction="column">
-          <Code fontSize="lg" display="block">
-            {text}
-          </Code>
-        </Flex>
-      ),
-    },
-    renderText: (text: any) => {
-      return text.split("\n").reduce((children: any[], textSegment: any, index: number) => {
-        return [...children, index > 0 && <br key={index} />, textSegment];
-      }, []);
-    },
-  };
 
   return (
     <Flex
@@ -256,7 +97,7 @@ export default function Post({
                 {post.author.name} &bull; {parseDate(post.date)}
               </Text>
             </HStack>
-            {documentToReactComponents(post.content.json, config)}
+            {documentToReactComponents(post.content.json, getConfig(post))}
           </Box>
         </>
       ) : (
@@ -310,10 +151,10 @@ export const getStaticProps: GetStaticProps = async ({ params, preview = false }
       query: getPreviewPostData,
       variables: { slug: params.slug },
     });
-    const previewPost = data?.postCollection?.items[0] || null;
+
     return {
       props: {
-        previewPost,
+        previewPost: data?.postCollection?.items[0] || null,
         slug: params.slug,
         preview,
       },
@@ -328,10 +169,8 @@ export const getStaticProps: GetStaticProps = async ({ params, preview = false }
 
   return {
     props: {
-      previewPost: null,
       initialApolloState: client.cache.extract(),
       slug: params.slug,
-      preview,
     },
     revalidate: 60,
   };
